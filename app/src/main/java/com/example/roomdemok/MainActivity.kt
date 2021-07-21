aff1b7d3b73a118a7d4b0f137fe10752b2930a6f
Ivan Petr <ivan.kiel@gmail.com>
2021-07-21 14:55:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/roomdemok/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.roomdemok
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
